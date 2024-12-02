963dde3d2c475f15a37117a8d12c5072a4d3131f
FernandoTejedorL <fernandotejedorlopez@gmail.com>
2024-12-02 20:59:04 +0100

    pending analogic

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,52 @@
 const rootStyles = document.documentElement.style;
+const hourDigiElement = document.getElementById('hour-d');
+const minDigiElement = document.getElementById('min-d');
+const weekDayElement = document.getElementById('weekday');
+const dayElement = document.getElementById('day');
+const monthElement = document.getElementById('month');
+const yearElement = document.getElementById('year');
+const week = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
+
+const allMonths = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+// Digital
+
+const setDigital = () => {
+  const date = new Date();
+  const minutes = date.getMinutes();
+  const hour = date.getHours();
+  const day = date.getDate();
+  const weekDay = week[date.getDay()];
+  const month = allMonths[date.getMonth()];
+  const year = date.getFullYear();
+  hourDigiElement.textContent = hour;
+  minDigiElement.textContent = minutes;
+  dayElement.textContent = day;
+  weekDayElement.textContent = weekDay;
+  monthElement.textContent = month;
+  yearElement.textContent = year;
+};
+setInterval(setDigital, 1000);
+
+//analógico
